838c8a66aed62db66d5c4cfa834a035bffe2135d
Sakibfootball <62951194+Sakibfootball@users.noreply.github.com>
2024-02-03 23:38:06 +0600

    style the active link in the nav

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,26 +1,39 @@
+"use client"
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { HiOutlineBugAnt } from "react-icons/hi2";
+import classNames from "classnames";
 
 const NavBar = () => {
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
   ];
+  const currentPath = usePathname();
   return (
     <nav className="flex space-x-5 border-b mb-5 px-4 h-12 items-center">
       <Link href="/">
-        <HiOutlineBugAnt size="2em" color="purple" />
+        <HiOutlineBugAnt size="2em" color="orange" />
       </Link>
       <ul className="flex space-x-5">
         {links.map((link) => (
           <Link
-            className="text-slate-500 hover:text-purple-900 transition-colors"
+            key={link.href}
+            className={classNames({
+                "text-orange-600": currentPath === link.href,
+                "text-slate-500": currentPath !== link.href,
+                "hover:text-orange-500 transition-colors": true,
+                
+            })}
             href={link.href}
           >
             {link.label}
           </Link>
         ))}
+        {/* {`${
+              link.href === currentPath ? "text-orange-600" : "text-slate-500"
+            } text-slate-500 hover:text-orange-400 transition-colors`} */}
       </ul>
     </nav>
   );
