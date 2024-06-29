ee2312c8fa707e29e102c5807bc48e0e53ff5f8e
sakibfootball <sakibhaider20@gmail.com>
2024-06-29 14:23:19 +0600

    add edit button complete

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -6,6 +6,7 @@ import { notFound } from "next/navigation";
 import React from "react";
 import Markdown from "react-markdown";
 import { Pencil2Icon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
 interface Props {
   params: { id: string };
@@ -30,10 +31,12 @@ const IssueDetailPage = async ({ params }: Props) => {
         </Card>
       </Box>
       <Box>
+        <Link href="#">
         <Button>
           <Pencil2Icon />
           Edit Issue
         </Button>
+        </Link>
       </Box>
     </Grid>
   );
